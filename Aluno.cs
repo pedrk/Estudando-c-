479aa3221a6d1ec9194ac97366eae43affba80d3
pedrk <pedro.kondrat.ismart@gmail.com>
2020-04-04 14:55:02 -0300

    Exercicio 1

--- Aluno.cs
+++ Aluno.cs
@@ -2,17 +2,36 @@ using System;
 
 namespace Sexta
 {
-    public class Aluno
+    public class Operacao
     {
         
         public string Soma(int n1, int n2) 
         {
-            int resultado = n1 + n2;
-            return $"O seu resultado é: {resultado}" ;
+            int somar = n1 + n2;
+            return $"O seu resultado é: {somar}" ;
             //return $"Seja bem vindo, {nome}";
-
         }
 
+        public string Sub(int n1, int n2)
+        {
+            int subtracao = n1 - n2;
+            return $"O seu resultado é: {subtracao}" ;
+        }
 
+        public string Mult(int n1, int n2)
+        {
+            int mult = n1 * n2;
+            return $"O seu resultado é: {mult}" ;
+        }
+
+        public string Div(int n1, int n2)
+        {
+            int divisao = n1 - n2;
+            return $"O seu resultado é: {divisao}" ;
+        }
+
+        
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,18 +6,70 @@ namespace Sexta
     {
         static void Main(string[] args)
         {
-            Aluno objAula = new Aluno(); 
-            
-            Console.WriteLine("Insira o primeiro numero: ");
-            int n1 = Int32.Parse(Console.ReadLine());
+            Operacao objAula = new Operacao(); 
 
-            Console.WriteLine("Insira o segundo numero: ");
-            int n2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("1 - Fazer operação");
+            int opcao = int.Parse(Console.ReadLine());
 
-            string result = objAula.Soma(n1, n2);
+            switch(opcao)
+            {
+                case 1:
+                    Operacao objOper = new Operacao();
+                    
+                    Console.WriteLine("Informar o valor um: ");
+                    int n1 = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Informar o valor dois: ");
+                    int n2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(result);
+                    string resultado = objOper.Soma(n1, n2);
+                    Console.WriteLine(resultado);
+                    break;
+
+                case 2:
+                    Operacao objOpera = new Operacao();
+
+                    Console.WriteLine("Informar o valor um: ");
+                    int num1 = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Informar o valor dois: ");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    string result = objOpera.Sub(num1, num2);
+                    Console.WriteLine(result);
+                    break;
+
+                case 3:
+                    Operacao objOperac = new Operacao();
+
+                    Console.WriteLine("Informar o valor um: ");
+                    int nume1 = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Informar o valor dois: ");
+                    int nume2 = int.Parse(Console.ReadLine());
+
+                    string resulta = objOperac.Mult(nume1, nume2);
+                    Console.WriteLine(resulta);
+                    break;
+                
+                case 4:
+                    Operacao objOperaca = new Operacao();
+
+                    Console.WriteLine("Informar o valor um: ");
+                    int numer1 = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Informar o valor dois: ");
+                    int numer2 = int.Parse(Console.ReadLine());
+
+                    string resultad = objOperaca.Div(numer1, numer2);
+                    Console.WriteLine(resultad);
+                    break;
+
+            }
+
+            Console.WriteLine("Fim da operação");
             Console.ReadKey();
         }
     }
 }
+
