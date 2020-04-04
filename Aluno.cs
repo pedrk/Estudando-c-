6a2b3e06b39d9d8a8bca274d6fdaee1abd543486
pedrk <pedro.kondrat.ismart@gmail.com>
2020-04-03 21:41:30 -0300

    Estudo inicial de tipos de dados

---
+++ Aluno.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Sexta
+{
+    public class Aluno
+    {
+        
+        public string Soma(int n1, int n2) 
+        {
+            int resultado = n1 + n2;
+            return $"O seu resultado é: {resultado}" ;
+            //return $"Seja bem vindo, {nome}";
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sexta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno objAula = new Aluno(); 
+            
+            Console.WriteLine("Insira o primeiro numero: ");
+            int n1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo numero: ");
+            int n2 = Int32.Parse(Console.ReadLine());
+
+            string result = objAula.Soma(n1, n2);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
